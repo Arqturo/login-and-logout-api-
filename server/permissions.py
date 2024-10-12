2e1f95a3756196d5c9ff1e49a7a53bee7fad88cc
Arqturo <117111402+Arqturo@users.noreply.github.com>
2024-10-12 19:17:14 -0400

    fix permissions

--- server/permissions.py
+++ server/permissions.py
@@ -1,5 +1,14 @@
 from rest_framework.permissions import BasePermission
-from .models import PageMaster, CustomUser  # Adjust the import based on your app structure
+from .models import PageMaster, CustomUser  # Adjust based on your app structure
+from django.contrib.auth.models import Permission
+
+
+class IsAuthenticatedUser(BasePermission):
+    """
+    Allows access only to authenticated users.
+    """
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
 
 
 class IsPageMaster(BasePermission):
@@ -7,7 +16,8 @@ class IsPageMaster(BasePermission):
     Allows access only to PageMaster users.
     """
     def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and isinstance(request.user, PageMaster)
+        return request.user and request.user.is_authenticated and request.user.has_perm('server.add_post')
+
 
 class IsCustomUser(BasePermission):
     """
